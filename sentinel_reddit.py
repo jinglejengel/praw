b388610aa99878eb4a02359eae3073a0dde70610
jose5732 <joe.engel@rackspace.com>
2014-02-10 18:29:27 -0600

    White spaces and description change

--- sentinel_reddit.py
+++ sentinel_reddit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Store reddit comments into a redis DB using praw and redis-py
+# Store reddit comments into a redis DB (backended with sentinel failover) using praw and redis-py
 #
 # Written by Joe Engel
 # Additional Contributers: Mark Lessel
@@ -18,24 +18,30 @@ master = db.master_for('mymaster', socket_timeout=0.1)
 # Create the reddit object, and pull comment stream from http://reddit.com/r/all/comments
 r = praw.Reddit('Comment Parsing by /u/joeskyyy')
 
+# Failover function to re-assign a master ina failover scenario
+def failover():
+        print "Error! Failover detected, trying to locate new master"
+        master = db.master_for('mymaster', socket_timeout=0.1)
+        time.sleep(1)
+        return master
+
 while True:
-        all_comments = r.get_comments('all')
+		all_comments = r.get_comments('all')
 
         # Loop through the list of comments returned and store their id, content, author, subreddit, and permalink
-        for comment in all_comments:
-                post = {}
-                post['_id'] = comment.id
-                post['comment'] = comment.body
-                post['author'] = str(comment.author)
-                post['subreddit'] = str(comment.subreddit)
-                post['permalink'] = comment.permalink
-                succeeded = False
-                while not succeeded:
-                        try:
-                                succeeded = master.rpush("comments", json.dumps(post))
-                        except:
-                                print "Error! Failover detected, trying to locate new master"
-                                master = db.master_for('mymaster', socket_timeout=0.1)
-                                time.sleep(1)
-                                continue
-                print "Inserted commentID: %s" % comment.id
+		for comment in all_comments:
+			post = {}
+			post['_id'] = comment.id
+			post['comment'] = comment.body
+			post['author'] = str(comment.author)
+			post['subreddit'] = str(comment.subreddit)
+			post['permalink'] = comment.permalink
+			# Catch an exception for failure before doing an insert
+			succeeded = False
+			while not succeeded:
+				try:
+					succeeded = master.rpush("comments", json.dumps(post))
+				except redis.exceptions.ResponseError:
+					master = failover()
+					continue
+			print "Inserted commentID: %s" % comment.id
